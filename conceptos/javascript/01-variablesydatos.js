117b69654018275e4d315771a7eefe36ae728cf1
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-20 20:22:11 -0500

    update

--- conceptos/javascript/01-variablesydatos.js
+++ conceptos/javascript/01-variablesydatos.js
@@ -1,6 +1,52 @@
 console.log("Hola Mundo");
 
 // variable
+// string - texto
 let nombre = "Jorge";
 
 console.log(nombre);
+
+// numero - number
+let edad = 31;
+
+console.log(edad);
+
+// Boolean - Booleano
+let meQuiere = false; //true o false
+
+console.log(meQuiere);
+
+//cambiar de tipo de dato
+edad = "treinta y uno";
+
+console.log(typeof edad);
+//UNA VARIABLE VA A PODER REFERENCIAR CASI CUALQUIER COSA EN JS
+
+let nota;
+// undefined, no esta definido
+console.log(nota);
+
+let nulo = null; //deberia haber algo pero no esta
+
+console.log(nulo);
+
+//Operaciones Aritméticas
+console.log("----Operaciones Aritméticas----");
+
+let a = 100;
+let b = 4;
+
+let suma = a + b;
+console.log("Suma", suma);
+
+let resta = a - b;
+console.log("Resta", resta);
+
+let Multiplicacion = a * b;
+console.log("Multiplicación", Multiplicacion);
+
+let Division = a / b;
+console.log("División", Division);
+
+let residuo = 105 % 4;
+console.log("Residuo", residuo);

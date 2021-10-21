80077cfdde166956f3079fac3af071c323aafe79
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-20 21:45:46 -0500

    update

--- conceptos/javascript/04-condicionales.js
+++ conceptos/javascript/04-condicionales.js
@@ -1,4 +1,6 @@
-let edad = 15;
+let edad = 20;
+let disfrazado = false;
+let comorbolidad = true;
 
 // if (edad > 18) {
 //     console.log("Puedes Vacunarte");
@@ -6,8 +8,19 @@ let edad = 15;
 //     console.log("Espera por favor");
 // }
 
-if (edad > 60) {
+// if (edad > 60) {
+//     console.log("Vacunate ahora - Pfizer");
+// } else if (edad > 18) {
+//     //pueden ser varios else if
+//     console.log("Vacunate ahora - Sinopharm");
+// } else {
+//     console.log("Espera por favor");
+// }
+
+if (edad > 60 || comorbolidad === true) {
     console.log("Vacunate ahora - Pfizer");
+} else if (edad > 18 && disfrazado === true) {
+    console.log("Vacunate con Sinopharm y entras al concurso");
 } else if (edad > 18) {
     console.log("Vacunate ahora - Sinopharm");
 } else {

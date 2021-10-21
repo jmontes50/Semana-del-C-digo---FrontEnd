85ef2f5e29c33be22518225213d3f364433ca7e4
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-20 20:53:12 -0500

    update

--- conceptos/javascript/02-funciones.js
+++ conceptos/javascript/02-funciones.js
@@ -3,9 +3,13 @@
 let constante = 2;
 
 function calcularArea(base, altura) {
+    // let constante = 2;
     let area = (base * altura) / constante;
     return area; //opcional
 }
 
 // invocar-llamar
 console.log(calcularArea(100, 50));
+
+// ambito o scope
+console.log(constante);

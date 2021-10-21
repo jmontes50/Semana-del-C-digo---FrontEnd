dc236727bcf494e77193c80afb35e930dd86f7e2
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 18:27:30 -0500

    update

---
+++ conceptos/javascript/06-objetos.js
@@ -0,0 +1,9 @@
+let mascota = {
+    nombre: "Backus",
+    raza: "Poddle",
+    edad: 13,
+    hobbies: ["Jugar", "Dormir", "Perseguir gatos"],
+    comer: function () {
+        console.log("ñam ñam ñam");
+    },
+};

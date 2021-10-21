c2a62a206c4869d18499331b044de78cfde94644
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-20 21:35:43 -0500

    update

---
+++ conceptos/javascript/04-condicionales.js
@@ -0,0 +1,15 @@
+let edad = 15;
+
+// if (edad > 18) {
+//     console.log("Puedes Vacunarte");
+// } else {
+//     console.log("Espera por favor");
+// }
+
+if (edad > 60) {
+    console.log("Vacunate ahora - Pfizer");
+} else if (edad > 18) {
+    console.log("Vacunate ahora - Sinopharm");
+} else {
+    console.log("Espera por favor");
+}

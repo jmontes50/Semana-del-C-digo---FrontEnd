e39e9e21d5ab8610e5808e505a5ff097620f4741
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-20 21:06:57 -0500

    update

---
+++ conceptos/javascript/03-arreglos.js
@@ -0,0 +1,19 @@
+// para declarar un arreglo/array, necesitamos ponerlos entre []
+// Todo elemento dentro de un arreglo va a tener una posición/indice que comienza siempre desde 0,1,2,3...
+let alumnos = ["Felipe", "Walter", "Enok", "Gabriela", "Karen", "Janet", "Hugo"];
+
+// console.log(alumnos);
+console.log(alumnos[4]);
+
+console.log(alumnos[2]);
+
+console.log(alumnos[6]);
+
+// longitud o length, la cantidad de elementos que yo tengo dentro de un arreglo, en este caso cuenta desde 1
+
+console.log(alumnos.length);
+
+//agregar nuevos items, al final
+alumnos.push("Alfredo");
+
+console.log(alumnos);

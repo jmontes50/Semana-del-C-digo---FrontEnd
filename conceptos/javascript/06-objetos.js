98b4cacf404f8aa07a10409415884b34fd269f17
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 18:33:01 -0500

    update

--- conceptos/javascript/06-objetos.js
+++ conceptos/javascript/06-objetos.js
@@ -7,3 +7,14 @@ let mascota = {
         console.log("ñam ñam ñam");
     },
 };
+
+console.log(mascota);
+// console.table(mascota);
+
+//objeto.propiedad
+
+console.log(mascota.nombre);
+console.log(mascota.edad);
+console.log(mascota.hobbies[2]);
+
+mascota.comer();

2e01ed68beb3b9a79528d8e27c92c21d408d7673
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-20 21:58:49 -0500

    update

---
+++ conceptos/javascript/05-for.js
@@ -0,0 +1,14 @@
+//for(inicializador, condicional, actualizacion){
+// codigo repetitivo
+// }
+// for(let i = 0; i < 10; i = i + 1)
+// for (let i = 0; i < 10; i++) {
+//     console.log("estoy repitiendome");
+//     console.log(i);
+// }
+
+let postres = ["Pie de limón", "Cheesecake", "Gelatina"];
+
+for (let i = 0; i < postres.length; i++) {
+    console.log(postres[i]);
+}

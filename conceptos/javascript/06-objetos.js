2a73b8fe322489ac7fe4d023be12f8b08c01f845
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 18:44:42 -0500

    update

--- conceptos/javascript/06-objetos.js
+++ conceptos/javascript/06-objetos.js
@@ -8,13 +8,31 @@ let mascota = {
     },
 };
 
-console.log(mascota);
-// console.table(mascota);
+// console.log(mascota);
+// // console.table(mascota);
 
-//objeto.propiedad
+// //objeto.propiedad
 
-console.log(mascota.nombre);
-console.log(mascota.edad);
-console.log(mascota.hobbies[2]);
+// console.log(mascota.nombre);
+// console.log(mascota.edad);
+// console.log(mascota.hobbies[2]);
 
-mascota.comer();
+// mascota.comer();
+
+let albumMusical = {
+    nombre: "After Hours",
+    fechaLanzamiento: "20/03/2020",
+    generos: ["Dream Pop", "Drum and bass"],
+    canciones: [
+        {
+            titulo: "Blinding Lights",
+            duracion: 3.22,
+            autores: ["Abel TesFaye", "Ahmad Balshe", "Jason Quenneville"],
+        },
+        {
+            titulo: "In your eyes",
+            duracion: 3.57,
+            autores: ["The weeknd", "Ahmad Balshe", "Max Martín"],
+        },
+    ],
+};

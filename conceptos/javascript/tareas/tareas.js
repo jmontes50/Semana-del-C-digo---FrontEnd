0a8a0e48abfb377bbba8915ed0b753adb891a426
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 21:40:00 -0500

    update

--- conceptos/javascript/tareas/tareas.js
+++ conceptos/javascript/tareas/tareas.js
@@ -6,14 +6,31 @@ let btnTarea = document.getElementById("btnTarea");
 let divContenido = document.getElementById("contenedor");
 
 // .innerHTML modifica el contenido HTML que tenga un elemento
-divContenido.innerHTML = "<h1>Título desde JS</h1>";
+// divContenido.innerHTML = "<h1>Título desde JS</h1>";
 
-inputTarea.value = "Comprar leche";
+let tareas = []; //almacenar las tareas por hacer
 
 //capturando eventos
 //addEventListener("evento", function(){})
 btnTarea.addEventListener("click", function () {
-    console.log("Me has hecho click!!");
     // en un input, podemos capturar su contenido/valor actual con la propiedad .value
-    console.log(inputTarea.value);
+    // console.log(inputTarea.value);
+    let nuevaTarea = {
+        texto: inputTarea.value,
+        fecha: new Date(), //fecha y hora actual, objeto
+    };
+    tareas.push(nuevaTarea); //agrega la nueva tarea
+    console.table(tareas); //observamos que tareas tengo
+    dibujarTareas(); //c/click ejecutamos dibujarTareas()
 });
+
+function dibujarTareas() {
+    let htmlTareas = "";
+    // item es c/items dentro de tarea
+    tareas.forEach(function (item) {
+        // transformar
+        // + concatenar/unir texto
+        htmlTareas = htmlTareas + `<p>${item.texto} - ${item.fecha.toLocaleDateString("es-PE")}</p>`;
+    });
+    divContenido.innerHTML = htmlTareas;
+}

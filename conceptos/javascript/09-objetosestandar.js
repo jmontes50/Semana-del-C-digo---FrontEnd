3a29a95aa813dc735be08cfcd70a764a5e3f9ebd
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 20:41:31 -0500

    update

---
+++ conceptos/javascript/09-objetosestandar.js
@@ -0,0 +1,16 @@
+let nombreCompleto = "Jorge Montesinos";
+
+console.log(nombreCompleto.toUpperCase());
+
+//TODO ES UN OBJETO
+//Tenemos muchas utilidades escondidas dentro de JS
+
+let hoy = new Date();
+console.log(hoy);
+
+let frutas = ["Kiwi", "Fresas", "Naranjas"];
+
+frutas.forEach(function (fruit) {
+    //lo va a repetir X veces según la cant de elementos/items que tenga mi arreglo
+    console.log(fruit);
+});

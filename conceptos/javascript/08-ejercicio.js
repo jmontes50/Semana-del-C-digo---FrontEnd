5a529581b8e88835d5130faa6eb332f02cc67bee
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 19:39:42 -0500

    update

---
+++ conceptos/javascript/08-ejercicio.js
@@ -0,0 +1,43 @@
+let productos = [
+    {
+        nombre: "Audifonos",
+        clave: 1,
+        materiaPrima: 20,
+    },
+    {
+        nombre: "Funda para movil",
+        clave: 2,
+        materiaPrima: 10,
+    },
+    {
+        nombre: "USB",
+        clave: 3,
+        materiaPrima: 15,
+    },
+    {
+        nombre: "Mouse Gamer",
+        clave: 6,
+        materiaPrima: 30,
+    },
+    {
+        nombre: "Teclado",
+        clave: 4,
+        materiaPrima: 120,
+    },
+    {
+        nombre: "Gráfica",
+        clave: 5,
+        materiaPrima: 200,
+    },
+];
+
+function calcularManoObra(clave, costoMP) {}
+
+function calcularCostos(listaProductos) {
+    let costoProd = 0;
+    for (let i = 0; i < listaProductos.length; i++) {
+        let costoMP = listaProductos[i].materiaPrima;
+        let clave = listaProductos[i].clave;
+        //
+    }
+}

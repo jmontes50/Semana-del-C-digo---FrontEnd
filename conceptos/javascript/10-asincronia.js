f0c8d4d86b15f11da443cbcb3eb3e786743e1f50
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 22:01:02 -0500

    update

---
+++ conceptos/javascript/10-asincronia.js
@@ -0,0 +1,11 @@
+//1. Javascript solo puede ejecutar una tarea a la vez
+
+//2. Asincronia?
+
+console.log("1. Hola amig@s");
+
+setTimeout(function () {
+    console.log("2. en que andan");
+}, 0);
+
+console.log("3. Espero esten bien");

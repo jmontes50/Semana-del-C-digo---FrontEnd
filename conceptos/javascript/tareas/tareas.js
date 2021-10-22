4753bf905accd8aeb2027da2c66eed70a3c4d808
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 21:04:39 -0500

    update

--- conceptos/javascript/tareas/tareas.js
+++ conceptos/javascript/tareas/tareas.js
@@ -4,3 +4,16 @@ let inputTarea = document.getElementById("inputTarea");
 let btnTarea = document.getElementById("btnTarea");
 
 let divContenido = document.getElementById("contenedor");
+
+// .innerHTML modifica el contenido HTML que tenga un elemento
+divContenido.innerHTML = "<h1>Título desde JS</h1>";
+
+inputTarea.value = "Comprar leche";
+
+//capturando eventos
+//addEventListener("evento", function(){})
+btnTarea.addEventListener("click", function () {
+    console.log("Me has hecho click!!");
+    // en un input, podemos capturar su contenido/valor actual con la propiedad .value
+    console.log(inputTarea.value);
+});

058022fc57d44752a56bdc4c9da769ecdeee3061
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 19:45:52 -0500

    update

--- conceptos/javascript/08-ejercicio.js
+++ conceptos/javascript/08-ejercicio.js
@@ -31,13 +31,23 @@ let productos = [
     },
 ];
 
-function calcularManoObra(clave, costoMP) {}
+function calcularManoObra(clave, costoMP) {
+    if (clave === 3 || clave === 4) {
+        return costoMP * 0.75; //se termina la función
+    } else if (clave === 1 || clave === 5) {
+        return costoMP * 0.8;
+    } else {
+        // se asume estas son claves 2 o 6
+        return costoMP * 0.85;
+    }
+}
 
+//PRINCIPAL
 function calcularCostos(listaProductos) {
     let costoProd = 0;
     for (let i = 0; i < listaProductos.length; i++) {
         let costoMP = listaProductos[i].materiaPrima;
         let clave = listaProductos[i].clave;
-        //
+        let costoMO = calcularManoObra(clave, costoMP);
     }
 }

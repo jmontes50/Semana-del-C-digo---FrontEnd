b67f044a5d200f775d9cb3a7899722060e94acc4
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-21 20:50:20 -0500

    update

---
+++ conceptos/javascript/tareas/tareas.js
@@ -0,0 +1,6 @@
+//creando referencias para los elementos que estan en el HTML
+let inputTarea = document.getElementById("inputTarea");
+
+let btnTarea = document.getElementById("btnTarea");
+
+let divContenido = document.getElementById("contenedor");

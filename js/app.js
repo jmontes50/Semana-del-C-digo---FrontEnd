ae0293ebfa673854bc0008b0195213dfe0d2c7ab
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-22 20:16:12 -0500

    update

--- js/app.js
+++ js/app.js
@@ -3,3 +3,19 @@ let btnPrueba = document.getElementById("btnPrueba");
 btnPrueba.addEventListener("click", function () {
     console.log("Click!");
 });
+
+async function buscarDeezer(busqueda) {
+    let configuracion = {
+        method: "GET",
+        // headers, rapid-api, privado
+        headers: {
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+            "x-rapidapi-key": "f6fd461a61mshff38733c3e43515p18e191jsn9e41ff4bce99",
+        },
+    };
+
+    try {
+        //fetch(url, configuracion)
+        let rpta = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${busqueda}`);
+    } catch (error) {}
+}

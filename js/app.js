c31a5228fdd4e821552c8ad9f184edb895cdb989
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-22 21:39:40 -0500

    update

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ let cerrar = document.getElementById("btnCerrar");
 let divCanciones = document.getElementById("divCanciones");
 
 btnPrueba.addEventListener("click", function () {
-    // buscarDeezer("x dinero");
+    buscarDeezer("x dinero");
     // elementosHTML tiene una propiedad .style
     modal.style.display = "block";
 });
@@ -31,7 +31,7 @@ async function buscarDeezer(busqueda) {
         //fetch(url, configuracion)
         let rpta = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${busqueda}`, configuracion);
         const datos = await rpta.json(); //convierte de TEXTO(json) a un Objeto de JS
-        console.table(datos.data);
+        console.log(datos.data);
     } catch (error) {
         console.log(error);
     }
@@ -45,18 +45,21 @@ function dibujarCanciones(arregloCanciones) {
         htmlCanciones =
             htmlCanciones +
             `<div class="cancion-container">
-			<div class="cancion">
-				<div class="cancion-img">
-					<img src="" alt="">
+				<div class="cancion">
+					<div class="cancion-img">
+						<img 
+							src="${cancion.artist.picture_medium}" 
+							alt="${cancion.title}"
+						>
+					</div>
+					<div class="cancion-content">
+						<h3>${cancion.title}</h3>
+						<p>${cancion.artist.name}</p>
+						<audio controls>
+							<source src="${cancion.preview}">
+						</audio>
+					</div>
 				</div>
-				<div class="cancion-content">
-					<h3></h3>
-					<p></p>
-					<audio controls>
-						<source src="">
-					</audio>
-				</div>
-			</div>
-		</div>`;
+			</div>`;
     });
 }
